1f4e98cf46a39453110a7bea802eb871b8eaedf2
Guilherme Sandi <guilherme_sandi@hotmail.com>
2024-02-07 15:54:03 -0300

    Swipeable menu

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import 'react-native-gesture-handler';
 
 import { StatusBar } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';}
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import { Routes } from './src/routes';
 import { Loading } from './src/components/Loading';
