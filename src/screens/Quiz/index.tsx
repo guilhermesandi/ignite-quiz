e72688c9b424b15ceedeaf00037b80591befd740
Guilherme Sandi <guilherme_sandi@hotmail.com>
2024-03-12 18:09:38 -0300

    Detectando Gestos

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -10,6 +10,7 @@ import Animated, {
   useAnimatedScrollHandler,
   Extrapolate,
 } from "react-native-reanimated";
+import { GestureDetector, Gesture } from 'react-native-gesture-handler';
 
 import { useNavigation, useRoute } from "@react-navigation/native";
 
@@ -43,6 +44,7 @@ export function Quiz() {
 
   const shake = useSharedValue(0);
   const scrollY = useSharedValue(0);
+  const cardPosition = useSharedValue(0);
 
   const { navigate } = useNavigation();
 
@@ -164,6 +166,12 @@ export function Quiz() {
     },
   });
 
+  const onPan = Gesture
+    .Pan()
+    .onUpdate((event) => {
+      cardPosition.value = event.translationX;
+    })
+
   useEffect(() => {
     const quizSelected = QUIZ.filter((item) => item.id === id)[0];
     setQuiz(quizSelected);
@@ -207,14 +215,16 @@ export function Quiz() {
           />
         </Animated.View>
 
-        <Animated.View style={shakeStyleAnimated}>
-          <Question
-            key={quiz.questions[currentQuestion].title}
-            question={quiz.questions[currentQuestion]}
-            alternativeSelected={alternativeSelected}
-            setAlternativeSelected={setAlternativeSelected}
-          />
-        </Animated.View>
+        <GestureDetector gesture={onPan}>
+          <Animated.View style={shakeStyleAnimated}>
+            <Question
+              key={quiz.questions[currentQuestion].title}
+              question={quiz.questions[currentQuestion]}
+              alternativeSelected={alternativeSelected}
+              setAlternativeSelected={setAlternativeSelected}
+            />
+          </Animated.View>
+        </GestureDetector>
 
         <View style={styles.footer}>
           <OutlineButton title="Parar" onPress={handleStop} />
