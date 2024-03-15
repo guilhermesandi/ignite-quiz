6a3d48afc0ff034ec5cb3225b7225be3d3567fd5
Guilherme Sandi <guilherme_sandi@hotmail.com>
2024-03-15 19:31:55 -0300

    Executando Javascript a partir da animação

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -9,6 +9,7 @@ import Animated, {
   Easing,
   useAnimatedScrollHandler,
   Extrapolate,
+  runOnJS,
 } from "react-native-reanimated";
 import { GestureDetector, Gesture } from "react-native-gesture-handler";
 
@@ -34,6 +35,7 @@ interface Params {
 type QuizProps = (typeof QUIZ)[0];
 
 const CARD_INCLINATION = 10;
+const CARD_SKIP_AREA = (-200);
 
 export function Quiz() {
   const [points, setPoints] = useState(0);
@@ -191,7 +193,11 @@ export function Quiz() {
         cardPosition.value = event.translationX;
       }
     })
-    .onEnd(() => {
+    .onEnd((event) => {
+      if (event.translationX < CARD_SKIP_AREA) {
+        runOnJS(handleNextQuestion)();
+      }
+
       cardPosition.value = withTiming(0);
     });
 
